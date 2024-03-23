d8b121025197535bf74091488367a863432477e6
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-03-23 23:40:42 +0900

    Update CNN.py

--- Capstone/CNN.py
+++ Capstone/CNN.py
@@ -1,1 +1,43 @@
-.
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=9, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=9, out_channels=18, kernel_size=3, stride=1)
+        self.fc1 = nn.Linear(5 * 5 * 18, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        x = x.view(-1, 5 * 5 * 18)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+cnn = CNN()
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(cnn.parameters(), lr=0.01)
+
+cnn.train()
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+
+        outputs = cnn(inputs)
+
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print('[%d, %d] loss: %.5f' % (epoch + 1, i + 1, running_loss / 100))
+            running_loss = 0.0
+
+
+cnn.eval()  # test case 학습 방지를 위함
