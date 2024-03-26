a3b2258dcaa6f251ae515768a71574e1fcc9a88e
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-03-26 23:44:41 +0900

    Update 여자남자구분.py

--- Capstone/여자남자구분.py
+++ Capstone/여자남자구분.py
@@ -13,10 +13,11 @@ transform = transforms.Compose([
 ])
 train_image_root = "C:/Users/wns20/PycharmProjects/얼굴인식/face_data"
 test_image_root = "C:/Users/wns20/PycharmProjects/얼굴인식/face_test"
-train_loader = ImageFolder(root=train_image_root, transform=transform)
+train_dataset = ImageFolder(root=train_image_root, transform=transform)
 test_loader = ImageFolder(root=test_image_root, transform=transform)
-print("데이터셋 크기:", len(train_loader))
-print("클래스:", train_loader.classes)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=5, shuffle=True)
+# print("데이터셋 크기:", len(train_loader))
+# print("클래스:", train_loader.classes)
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -24,8 +25,8 @@ class CNN(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=9, out_channels=18, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(in_channels=18, out_channels=27, kernel_size=3, stride=1, padding=1)
         self.conv4 = nn.Conv2d(in_channels=27, out_channels=36, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(36, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.fc1 = nn.Linear(36, 100)
+        self.fc2 = nn.Linear(100, 2)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -38,6 +39,7 @@ class CNN(nn.Module):
         x = F.max_pool2d(x, kernel_size=2, stride=2)
         x = F.relu(self.fc1(x.view(-1, 36)))
         x = self.fc2(x)
+        x = F.softmax(x, dim=1)
         return x
 
 cnn = CNN()
@@ -45,33 +47,32 @@ criterion = torch.nn.CrossEntropyLoss()
 optimizer = optim.SGD(cnn.parameters(), lr=0.01)
 
 cnn.train()
-for epoch in range(1000):
+for epoch in range(10000):
     running_loss = 0.0
     for i, data in enumerate(train_loader):
         inputs, labels = data
-        inputs = inputs.unsqueeze(1)  # 배치 차원을 1로 추가
         optimizer.zero_grad()
 
         outputs = cnn(inputs)
 
-        loss = criterion(outputs, torch.LongTensor([labels]))
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
     if epoch % 100 == 99:
-        print('[%d] loss: %.5f' % (epoch + 1, running_loss / 100))
+        print('[%d] loss: %.10f' % (epoch + 1, running_loss / 100))
         running_loss = 0.0
 
 
 cnn.eval()
-test_loss = 0
 correct = 0
 with torch.no_grad():
     for data, target in test_loader:
         data = data.unsqueeze(1)
         output = cnn(data)
-        test_loss += criterion(output, torch.tensor([target])).item()
         pred = output.argmax(dim=1, keepdim=True)
+
+        print("예측률 :", output[0][0], ",", output[0][1])
         correct += pred.eq(torch.LongTensor([target])).sum().item()
 print(correct)
