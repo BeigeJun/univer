f128c009644d941c4dec0d60f7999d10bccd7ad0
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-04-29 21:52:08 +0900

    Update Make_3D_Pos.py

--- Capstone/Make_3D_Pos.py
+++ Capstone/Make_3D_Pos.py
@@ -1,15 +1,15 @@
 import numpy as np
 from PIL import Image
 import torch
-import torchvision
+# import torchvision
 from torchvision import models
-import torchvision.transforms as T
+import torchvision.transforms as t
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
-from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import Axes3D
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # CPU to GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = models.detection.keypointrcnn_resnet50_fpn(pretrained=True).to(device).eval()
 
 IMAGE_SIZE = 800
@@ -20,8 +20,8 @@ img_side = Image.open('C:/Users/wns20/Desktop/4.jpg')
 img_side = img_side.resize((IMAGE_SIZE, int(img_side.height * IMAGE_SIZE / img_side.width)))
 image_width, image_height = img_front.size
 
-trf = T.Compose([
-    T.ToTensor()
+trf = t.Compose([
+    t.ToTensor()
 ])
 
 codes = [
@@ -30,77 +30,83 @@ codes = [
   Path.LINETO
 ]
 
-def Make_sceleton(img):
+
+def make_sceleton(img):
     input_img = trf(img).to(device)
     out = model([input_img])[0]
     fig, ax = plt.subplots(1)
     ax.imshow(img)
-    KEY = torch.zeros((17, 3))
-    THRESHOLD = 0.9
-    for box, score, keypoints in zip(out['boxes'], out['scores'], out['keypoints']):
-        print("!")
+    key = torch.zeros((17, 3))
+    threshold = 0.9
+    for box, score, points in zip(out['boxes'], out['scores'], out['keypoints']):
         score = score.detach().cpu().numpy()
-        if score < THRESHOLD:
+        if score < threshold:
             continue
         box = box.detach().cpu().numpy()
-        keypoints = keypoints.detach().cpu().numpy()[:, :2]
+        points = points.detach().cpu().numpy()[:, :2]
 
         rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=2, edgecolor='white',
                                  facecolor='none')
         ax.add_patch(rect)
         for i in range(2):
-            path = Path(keypoints[5+i:10+i:2], codes)
+            path = Path(points[5+i:10+i:2], codes)
             line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='red')
             ax.add_patch(line)
 
         for i in range(2):
-            path = Path(keypoints[11+i:16+i:2], codes)
+            path = Path(points[11+i:16+i:2], codes)
             line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='red')
             ax.add_patch(line)
 
-        for i, k in enumerate(keypoints):
+        for i, k in enumerate(points):
             if i < 5:
-                RADIUS = 5
-                FACE_COLOR = 'yellow'
+                radius = 5
+                face_color = 'yellow'
             else:
-                RADIUS = 10
-                FACE_COLOR = 'red'
-            circle = patches.Circle((k[0], k[1]), radius=RADIUS, facecolor=FACE_COLOR)
+                radius = 10
+                face_color = 'red'
+            circle = patches.Circle((k[0], k[1]), radius=radius, facecolor=face_color)
             ax.add_patch(circle)
-        print(keypoints.shape)
-        KEY = keypoints
-    plt.show()
-    return KEY
 
-def Make_3D_sceleton(sceleton_1, sceleton_2):
-    print(sceleton_1)
-    print(sceleton_2)
-    Make_3D_pos = np.zeros((len(sceleton_1), 3))
+        key = points
+    plt.show()
+    return key
+
+
+def make_3d_sceleton(sceleton_1, sceleton_2):
+    make_3d = np.zeros((len(sceleton_1), 3))
     extract_x = sceleton_1[:, 0]
     extract_y = sceleton_1[:, 1]
     extract_z = sceleton_2[:, 0]
     for i in range(len(extract_x)):
-        Make_3D_pos[i][0] = extract_x[i]
-        Make_3D_pos[i][1] = extract_z[i]
-        Make_3D_pos[i][2] = extract_y[i]
-    return Make_3D_pos
+        make_3d[i][0] = extract_x[i]
+        make_3d[i][1] = extract_z[i]
+        make_3d[i][2] = extract_y[i]
+    return make_3d
 
-def Show_3D_Pos(Pos):
+
+def show_3d_pos(pos):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(Pos[:, 0], Pos[:, 1], Pos[:, 2])
+    ax.scatter(pos[:, 0], pos[:, 1], pos[:, 2])
+    head = (pos[3] + pos[4]) / 2
+    ax.scatter(head[0], head[1], head[2], s=250)
+    for i in range(5, 7, 1):
+        ax.plot(pos[[i, i+2], 0], pos[[i, i+2], 1], pos[[i, i+2], 2], color='blue')
+        ax.plot(pos[[i+2, i+4], 0], pos[[i+2, i+4], 1], pos[[i+2, i+4], 2], color='blue')
+    for i in range(11, 13, 1):
+        ax.plot(pos[[i, i+2], 0], pos[[i, i+2], 1], pos[[i, i+2], 2], color='green')
+        ax.plot(pos[[i+2, i+4], 0], pos[[i+2, i+4], 1], pos[[i+2, i+4], 2], color='green')
 
-    ax.plot(Pos[[5, 7], 0], Pos[[5, 7], 1], Pos[[5, 7], 2], color='blue')
-    ax.plot(Pos[[7, 9], 0], Pos[[7, 9], 1], Pos[[7, 9], 2], color='blue')
+    ax.plot([pos[5, 0], pos[6, 0]], [pos[5, 1], pos[6, 1]], [pos[5, 2], pos[6, 2]], color='blue')
 
-    ax.plot(Pos[[6, 8], 0], Pos[[6, 8], 1], Pos[[6, 8], 2], color='blue')
-    ax.plot(Pos[[8, 10], 0], Pos[[8, 10], 1], Pos[[8, 10], 2], color='blue')
+    shoulder_mid = (pos[5] + pos[6]) / 2
+    ax.plot([shoulder_mid[0], head[0]], [shoulder_mid[1], head[1]], [shoulder_mid[2], head[2]], color='blue')
 
-    ax.plot(Pos[[11, 13], 0], Pos[[11, 13], 1], Pos[[11, 13], 2], color='green')
-    ax.plot(Pos[[13, 15], 0], Pos[[13, 15], 1], Pos[[13, 15], 2], color='green')
+    ax.plot([pos[5, 0], pos[11, 0]], [pos[5, 1], pos[11, 1]], [pos[5, 2], pos[11, 2]], color='green')
+    ax.plot([pos[6, 0], pos[12, 0]], [pos[6, 1], pos[12, 1]], [pos[6, 2], pos[12, 2]], color='green')
 
-    ax.plot(Pos[[12, 14], 0], Pos[[12, 14], 1], Pos[[12, 14], 2], color='green')
-    ax.plot(Pos[[14, 16], 0], Pos[[14, 16], 1], Pos[[14, 16], 2], color='green')
+    ax.plot([pos[11, 0], pos[12, 0]], [pos[11, 1], pos[12, 1]], [pos[11, 2], pos[12, 2]], color='green')
 
     ax.set_xlabel('Front')
     ax.set_ylabel('Side')
@@ -111,11 +117,10 @@ def Show_3D_Pos(Pos):
     plt.show()
 
 
-
-Sceleton_1 = Make_sceleton(img_front)
-Sceleton_2 = Make_sceleton(img_side)
-Maked_3D_Pos = Make_3D_sceleton(Sceleton_1,Sceleton_2)
-Show_3D_Pos(Maked_3D_Pos)
+Sceleton_1 = make_sceleton(img_front)
+Sceleton_2 = make_sceleton(img_side)
+make_3d_pos = make_3d_sceleton(Sceleton_1, Sceleton_2)
+show_3d_pos(make_3d_pos)
 
 
 # keypoint_names = {
